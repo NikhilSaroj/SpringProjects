ee5e669a1635b5d660550fe1debb2f5fbf64afe4
NikhilS01 <NikhilS01@G09INXNP33123X1.g09.fujitsu.local>
2023-01-31 10:45:02 +0530

    Inner Bean concept execution from fujitsu laptop

---
+++ InnerBeans/src/com/tutorialspoint/SpellChecker.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint;
+
+public class SpellChecker {
+
+	public SpellChecker() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Inside SpellChecker constructor");
+	}
+	
+	public void spellChecking() {
+		System.out.println("Inside SpellChecking method");
+	}
+
+}
