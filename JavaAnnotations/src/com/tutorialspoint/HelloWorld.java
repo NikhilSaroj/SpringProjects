1978e14cec20bd699681b4d782bf8a3725172c59
NikhilS01 <NikhilS01@G09INXNP33123X1.g09.fujitsu.local>
2023-02-02 10:40:12 +0530

    File updated checking

--- JavaAnnotations/src/com/tutorialspoint/HelloWorld.java
+++ JavaAnnotations/src/com/tutorialspoint/HelloWorld.java
@@ -11,6 +11,11 @@ public class HelloWorld {
 
 	public void setMessage(String message) {
 		this.message = message;
+		
+	}
+	
+	public void justCheck() {
+		System.out.println("Checking updating file on git");
 	}
 	
 }
