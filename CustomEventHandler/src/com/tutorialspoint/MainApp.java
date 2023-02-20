ce5fb152b16af57ccbe03449016e6ce14a086255
NikhilS01 <NikhilS01@G09INXNP33123X1.g09.fujitsu.local>
2023-02-20 13:01:24 +0530

    Custom Event Handler

---
+++ CustomEventHandler/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+		CustomEventPublisher cvp =(CustomEventPublisher) context.getBean("customEventPublisher");
+	    cvp.publish();  
+	    cvp.publish();
+	}
+
+}
